13741dae5e36348e696d9c669d3bb33d849bceac
Beebbbyy <lacsonrica@gmail.com>
2022-02-21 14:40:54 +0800

    initial commit

--- 009 Python Numpy/array_lesson.py
+++ 009 Python Numpy/array_lesson.py
@@ -1,3 +1,4 @@
+from traceback import print_tb
 import numpy as np
 
 #Numpy array has a homogenous data type
@@ -22,22 +23,50 @@ import numpy as np
 # # print(array_d.shape)
 
 # Reshaping & Indexing NumPy Array
-array_a=np.array([[1,2,3],[4,5,6]], dtype=float)
-print(array_a)
-# print(array_a.size)
+# array_a=np.array([[1,2,3],[4,5,6]], dtype=float)
+# print(array_a)
+# # print(array_a.size)
 
-# array_b=array_a.T
-# # print(array_b)
-# print(array_b[0])
-# print(array_b[:,1])
-# print(array_b[0,0])
+# # array_b=array_a.T
+# # # print(array_b)
+# # print(array_b[0])
+# # print(array_b[:,1])
+# # print(array_b[0,0])
 
-array_b=np.zeros((3,3))
-print(array_b)
+# array_b=np.zeros((3,3))
+# print(array_b)
 
-array_c=np.ones((3,3))
-print(array_c)
-array_d=np.full((3,3,),5)
-print(array_d)
-array_e=np.random.random((3,4))
-print(array_e)
+# array_c=np.ones((3,3))
+# print(array_c)
+# array_d=np.full((3,3,),5)
+# print(array_d)
+# array_e=np.random.random((3,4))
+# print(array_e)
+
+row_1= [1,2,3,4,5]
+row_2= [6,7,8,9,10]
+row_3= [11,12,13,14,15]
+row_4= [16,17,18,19,20]
+row_5= [21,22,23,24,25]
+
+test_data=np.array([row_1,row_2,row_3,row_4,row_5])
+print(test_data)
+
+#slice
+print(test_data[:,2:4:1]) #start:ending value:stepsize
+#negative index
+print(test_data[:,-2:-4:-1])
+
+#boolean 
+greater_than_five=test_data> 5
+print(greater_than_five)
+print(test_data[greater_than_five])
+
+#where
+drop_under_5_array = np.where(test_data>5, test_data,0)
+print(drop_under_5_array)
+
+#logical and
+drop_under_5_and_over_20=np.logical_and(test_data>5, test_data<20)
+print(drop_under_5_and_over_20)
+print(test_data[drop_under_5_and_over_20])
